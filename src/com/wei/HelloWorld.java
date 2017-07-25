c64e90581e753883e8691f475663e5f70cc9cd16
魏晓武 <xiaowu_wei@jyc99.com>
2017-07-25 20:27:13 +0800

    first

---
+++ src/com/wei/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.wei;
+/**   
+* @Description: TODO
+* @author weixw   
+* @date 2017年7月25日 
+* @since 1.0.0   
+*/
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+}
