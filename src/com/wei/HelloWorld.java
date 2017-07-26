c7b89176431fff420fd1f4f3b3a3e8de55656894
魏晓武 <xiaowu_wei@jyc99.com>
2017-07-26 10:27:59 +0800

    dev2

--- src/com/wei/HelloWorld.java
+++ src/com/wei/HelloWorld.java
@@ -9,5 +9,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 		System.out.println("HelloWorld dev");
+		System.out.println("HelloWorld dev2");
 	}
 }
